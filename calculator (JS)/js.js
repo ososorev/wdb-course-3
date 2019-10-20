d90e67d76b96cb666cecbaa49c5af8b7bb87c84a
vermillion23 <den4ik.frolov@gmail.com>
2019-10-20 18:50:48 +0300

    JavaScript calculator with CSS styles.

---
+++ calculator (JS)/js.js
@@ -0,0 +1,39 @@
+const number1 = document.getElementById('number1')
+const number2 = document.getElementById('number2')
+const operator = document.getElementById('operator')
+const resultButton = document.getElementById('resultButton')
+
+
+let operate = function () {
+
+    let result = 0
+
+    switch (operator.value) {
+        case 'multiply': // if operator.value = multiply
+            result = parseInt(number1.value) * parseInt(number2.value)
+            break
+
+        case 'divide': // if operator.value = divide
+            if (parseInt(number2.value) === 0) {
+                alert('Error: you cannot divide by 0')
+                break
+            }    
+                result = parseInt(number1.value) / parseInt(number2.value)
+            
+            break
+
+        case 'sumUp': // if operator.value = sumUp
+            result = parseInt(number1.value) + parseInt(number2.value)
+            break
+
+        case 'subtract': // if operator.value = subtract
+            result = parseInt(number1.value) - parseInt(number2.value)
+            break
+    }
+
+    let outputField = document.getElementById("result")
+    outputField.value = result
+
+}
+
+document.getElementById('resultButton').addEventListener('click', operate)   
