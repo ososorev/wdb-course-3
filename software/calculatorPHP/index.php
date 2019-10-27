97f4875314c483d819d2514a50638b53d520722d
Margo <mnchek@mail.ru>
2019-10-27 17:09:46 +0300

    removed redundant files

--- software/calculatorPHP/index.php
+++ software/calculatorPHP/index.php
@@ -17,7 +17,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="stylePhp.css">
+        <link rel="stylesheet" href="style.css">
         <title>Calculator</title>
     </head>
     <body>
