c92596255503d361c2d979e3123f59996b31e16a
olikik <olgam299@gmail.com>
2019-10-28 23:38:16 +0300

    калькулятор ajax

---
+++ calcajax/sendToServer.js
@@ -0,0 +1,6 @@
+function sendToServer(event) {
+    event.preventDefault();
+    fetch("calculation.php", {method: "POST", body: new FormData(document.forms[0])})
+        .then(response => response.text())
+        .then(text => {document.querySelector('#result').innerHTML = text});
+}
