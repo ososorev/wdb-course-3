31b30756d46518231bfcd169abb1ad8d9e3c5a0a
olikik <olgam299@gmail.com>
2019-10-31 02:09:28 +0300

    Калькулятор с сохранение в базу данных и выводом последних 5 операций

---
+++ calcdb/calculation.php
@@ -0,0 +1,12 @@
+<?php
+    require_once ("functions.php");
+    $number1 = $_REQUEST['number1'];
+    $number2 = $_REQUEST['number2'];
+    $operator = $_REQUEST['operator'];
+    $calcRes = calculationResult($number1,$number2,$operator) ;
+    echo addCorrectResultToDb($number1,$number2, $calcRes, $operator);
+    echo "Результат: ".$calcRes.fiveLastStr();
+    ?>
+
+
+
---
+++ calcdb/functions.php
@@ -0,0 +1,51 @@
+<?php
+    function calculationResult($number1,$number2,$operator){
+        if (is_numeric($number1) && is_numeric($number2)) {
+            switch ($operator) {
+                case '+':
+                    $result = $number1 + $number2;
+                    break;
+                case '-':
+                    $result = $number1 - $number2;
+                    break;
+                case '/':
+                    $result = ($number2 == 0) ? "На ноль делить низя." : $number1 / $number2;
+                    break;
+                case '*':
+                    $result = $number1 * $number2;
+                    break;
+            }
+        } else {
+            $result = "Похоже вы ввели не числа...";
+        }
+        return $result;
+    }
+function addToDataBase($number_1, $number_2, $result, $operator){
+    $connection = connectDataBase('calculation_archive');
+    mysqli_query($connection,
+        "INSERT INTO data(number_1, number_2, result, operator) 
+                VALUES ('$number_1','$number_2','$result', '$operator')");
+    return mysqli_error($connection);
+}
+
+function connectDataBase($nameDataBase){
+
+        return $connection = mysqli_connect('localhost','root','', $nameDataBase);
+}
+
+function addCorrectResultToDb($number_1, $number_2, $result, $operator){
+        if(is_numeric($result)){
+            addToDataBase($number_1, $number_2, $result, $operator);
+        }
+}
+
+function fiveLastStr(){
+    $connection = connectDataBase('calculation_archive');
+    $resultSelect = mysqli_query($connection,"SELECT * FROM data ORDER BY id DESC LIMIT 5;");
+    foreach ($resultSelect as $row){
+        $result = $result.'<p>'.$row['number_1'].' '.$row['operator'].' '.$row['number_2'].' = '.$row['result'].'</p>';
+    }
+    return $result;
+}
+
+?>
