8c2564eecae5a63c287df287f415fa7150dd9e57
FivePood <ps220790@gmail.com>
2019-11-02 16:31:25 +0300

    С пятью последними результатами

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+	$connection = mysqli_connect("localhost", "root", "", "calc");
 	$s1 = (int)$_REQUEST ["first"];
 	$s2 = (int)$_REQUEST ["two"];
 	$act = $_REQUEST ["produce"];
@@ -6,24 +7,30 @@
 	$result = "Результат";
 	if (!empty($out)){
     	$result = calc($s1, $s2, $act);
-		echo $result;
+		echo "$result<br/>";
 	}
 	function calc($s1, $s2, $act){
-		if ($act == "+ (Сложение)") {
+		if ($act == "+") {
 			$result = $s1+$s2;
 		}
-		else if ($act == "- (Вычитание)") {
+		else if ($act == "-") {
 			$result = $s1-$s2;
 		}
-		else if ($act == "* (Умножение)") {
+		else if ($act == "*") {
 			$result = $s1*$s2;
 		}
-		else if ($act == "/ (Деление)" && $s2 != "0") {
+		else if ($act == "/" && $s2 != "0") {
 			$result = $s1/$s2;
 		}
 		else {
 			$result = "На ноль делить нельзя";
 		}
 		return($result);
+	}
+	mysqli_query($connection, "INSERT INTO calculator(first, produce, two, output) VALUES ('$s1', '$act', '$s2', '$result')");
+	if ($resource = mysqli_query($connection, 'SELECT * FROM calculator ORDER BY id DESC LIMIT 5')) {
+    	while( $row = mysqli_fetch_assoc($resource) ){
+			printf("%s)   %s %s %s = %s<br/>", $row['id'], $row['first'], $row['produce'], $row['two'], $row['output']);
+		}
 	}
 ?>
