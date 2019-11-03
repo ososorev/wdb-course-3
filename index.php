14e342f02f2b6739d1ea054b1b23e3225c945f32
FivePood <ps220790@gmail.com>
2019-11-03 22:00:14 +0300

    с сессей

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?php 
-	$connection = mysqli_connect("localhost", "root", "", "calc");
+	session_start();
+	$i= count($_SESSION["arr"]);
 	$s1 = (int)$_REQUEST ["first"];
 	$s2 = (int)$_REQUEST ["two"];
 	$act = $_REQUEST ["produce"];
 	$out = $_REQUEST ["produce"];
 	$result = "Результат";
 	if (!empty($out)){
-    	$result = calc($s1, $s2, $act);
+		$result = calc($s1, $s2, $act);
 		echo "$result<br/>";
+		$arr = array("$s1", "$act", "$s2", "$result");
+		$i = count($_SESSION["arr"]);
+		$_SESSION["arr"][$i] = $arr;
+		if ($i<5){
+		for ($s3 = 0; $s3<$i+1; $s3++){
+			printf("%s %s %s = %s<br/>", $_SESSION["arr"][$s3][0], $_SESSION["arr"][$s3][1], $_SESSION["arr"][$s3][2], $_SESSION["arr"][$s3][3]);
+			}
+        }
+		else{
+			for ($s3 = $i-4; $s3<$i+1; $s3++){
+				printf("%s %s %s = %s<br/>", $_SESSION["arr"][$s3][0], $_SESSION["arr"][$s3][1], $_SESSION["arr"][$s3][2], $_SESSION["arr"][$s3][3]);
+			}
+		}
 	}
 	function calc($s1, $s2, $act){
 		if ($act == "+") {
@@ -26,11 +40,5 @@
 			$result = "На ноль делить нельзя";
 		}
 		return($result);
-	}
-	mysqli_query($connection, "INSERT INTO calculator(first, produce, two, output) VALUES ('$s1', '$act', '$s2', '$result')");
-	if ($resource = mysqli_query($connection, 'SELECT * FROM calculator ORDER BY id DESC LIMIT 5')) {
-    	while( $row = mysqli_fetch_assoc($resource) ){
-			printf("%s)   %s %s %s = %s<br/>", $row['id'], $row['first'], $row['produce'], $row['two'], $row['output']);
-		}
 	}
 ?>
