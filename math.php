5da3b0a56e2fa49f82b776a122d17298c71e117a
BillyBones <useemy@gmail.com>
2019-11-06 01:02:15 +0300

    Добавлен класс для работы с SQL

--- math.php
+++ math.php
@@ -5,6 +5,28 @@
 	$action=$_REQUEST['select'];
 	$result="Здесь будт результат";
 
+	class dataBase {
+		private $conn;
+		private $chislo_1;
+		private $action;
+		private $chislo_2;
+		private $result;
+
+		function __construct($conn, $chislo_1, $action, $chislo_2, $result){
+			$this->conn= $conn;
+			$this->chislo_1= $chislo_1;
+			$this->action= $action;
+			$this->chislo_2= $chislo_2;
+			$this->result= $result;
+		}
+		
+		function insertData() {
+			$sql ="INSERT INTO calc(first, action, second, result) VALUES ('$this->chislo_1', '$this->action', '$this->chislo_2', '$this->result')";
+			mysqli_query($this->conn, $sql);
+		}
+	}
+
+
 	function math($a, $b, $act){				
 				if ($act == "+"){
 					$res = $a + $b;
@@ -26,11 +48,12 @@
 		return $res;
 	}
 
+
 	if (!empty($action)){
 		$result = math($chislo_1, $chislo_2, $action);
-
+		$firstRequest = new dataBase($connection, $chislo_1, $action, $chislo_2, $result);
+		$firstRequest-> insertData();	
 	}
-	mysqli_query($connection, "INSERT INTO calc(first, action, second, result) VALUES ('$chislo_1', '$action', '$chislo_2', '$result')");
 
 	if ($rezilt= mysqli_query($connection, 'SELECT * FROM calc ORDER BY id DESC LIMIT 5')){
  	while( $row = mysqli_fetch_assoc($rezilt))
@@ -38,4 +61,5 @@
             printf("%s %s %s %s = %s<br/>",$row['id'], $row['first'], $row['action'], $row['second'], $row['result']);
 		}
 	}
+
 ?>
