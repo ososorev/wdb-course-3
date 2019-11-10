78245e58086b304fb97be31a5ca89669fd0e7196
FivePood <ps220790@gmail.com>
2019-11-10 17:56:03 +0300

    Форма регистрации с БД

--- index.php
+++ index.php
@@ -1,74 +1,29 @@
 <?php 
-	$connection = mysqli_connect("localhost", "root", "", "calc");
-	$first = (int)$_REQUEST ["first"];
-	$second = (int)$_REQUEST ["second"];
-	$act = $_REQUEST ["produce"];
-	$out = $_REQUEST ["produce"];
-	$result = "Результат";
-	if (!empty($out)){
-    	$result = calc($first, $second, $act);
-		echo "$result<br/>";
+	$username = $_REQUEST ["username"];
+	$password = $_REQUEST ["password"];
+	$confirmPassword = $_REQUEST ["confirmPassword"];
+	$email = $_REQUEST ["email"];
+	if (!empty($username)&&!empty($password)&&!empty($confirmPassword)&&!empty($email)){
+    	$result = validate();
+		echo "$result";
 	}
-	function calc($first, $second, $act){					
-		if ($act == "+") {
-			$result = $first+$second;
-		}
-		else if ($act == "-") {
-			$result = $first-$second;
-		}
-		else if ($act == "*") {
-			$result = $first*$second;
-		}
-		else if ($act == "/" && $second != "0") {
-			$result = $first/$second;
-		}
+	function validate() {					
+		if ($password == $confirmPassword) {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$result = "Successful!";
+			} 
+			else if (1==1){
+				$result = "Invalid email";
+			}
 		else {
-			$result = "На ноль делить нельзя";
+			$result = "Invalid password";
 		}
 		return($result);
-	}
-	class DataBase {
-		public $connect;
-			public $first;
-			public $act;	
-			public $second;
-			public $result;
-				
-		function __construct($connect, $first, $act, $second, $result) {
-			$this->connect = $connect;
-			$this->first = $first;
-			$this->produce = $act;	
-			$this->second = $second;
-			$this->output = $result;
-		}
-		public function insert() {
-			$insert = "INSERT INTO calculator(first, produce, second, output)";
-			$insert .= "VALUES ('$this->first',";
-            $insert .= "'$this->produce',";
-            $insert .= "'$this->second',";
-            $insert .= "'$this->output')";
- 			mysqli_query($this->connect, $insert);
 		}
 	}
-	$oneDataBase = new DataBase($connection, $first, $act, $second, $result);
-	$oneDataBase->insert();
-
-
-$dataSource = 'mysql:dbname=calc;host=localhost';
-$user = 'root';
-$password = '';
-$db = new PDO($dataSource, $user, $password);
-//$db->exec("INSERT INTO calculator(first, produce, second, output) VALUES ('$first', '$act', '$second', '$result')");
-$resource = $db->query("SELECT * FROM calculator ORDER BY id DESC LIMIT 5");
-while($row = $resource->fetch()) {
- printf("%s)   %s %s %s = %s<br/>", $row['id'], $row['first'], $row['produce'], $row['second'], $row['output']);
-}
-
-
-//	mysqli_query($connection, "INSERT INTO calculator(first, produce, second, output) VALUES ('$first', '$act', '$second', '$result')");
-//	if ($resource = mysqli_query($connection, 'SELECT * FROM calculator ORDER BY id DESC LIMIT 5')) {
-//    	while( $row = mysqli_fetch_assoc($resource) ){
-//			printf("%s)   %s %s %s = %s<br/>", $row['id'], $row['first'], $row['produce'], $row['second'], $row['output']);
-//		}
-//	}
+	$dataSource = 'mysql:dbname=registration;host=localhost';
+	$user = 'root';
+	$password = '';
+	$db = new PDO($dataSource, $user, $password);
+	$db -> exec("INSERT INTO form(username, password, email) VALUES ('$username', MD5('$password'), '$email')");
 ?>
