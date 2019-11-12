53667d1f9814dd444010e194ae0d2a8d8ce50e08
FivePood <ps220790@gmail.com>
2019-11-12 22:24:17 +0300

    Теперь записывает в БД

--- index.php
+++ index.php
@@ -1,29 +1,33 @@
 <?php 
+//	$dataSource = 'mysql:dbname=registration;host=localhost';
+//	$user = 'root';
+//	$pass = '';
+//	$db = new PDO($dataSource, $user, $pass);
+//	$db->exec("INSERT INTO regist(username, password, email) VALUES ('$username', '$password', '$email')");
+
 	$username = $_REQUEST ["username"];
 	$password = $_REQUEST ["password"];
 	$confirmPassword = $_REQUEST ["confirmPassword"];
 	$email = $_REQUEST ["email"];
+
 	if (!empty($username)&&!empty($password)&&!empty($confirmPassword)&&!empty($email)){
     	$result = validate();
-		echo "$result";
+		echo ($result);
 	}
 	function validate() {					
-		if ($password == $confirmPassword) {
-			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//		if ($password == $confirmPassword) {
+//			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$result = "Successful!";
-			} 
-			else if (1==1){
-				$result = "Invalid email";
-			}
-		else {
-			$result = "Invalid password";
-		}
+//			} 
+//			else if (1==1){
+//				$result = "Invalid email";
+//			}
+//		else {
+//			$result = "Invalid password";
+//		}
 		return($result);
 		}
-	}
-	$dataSource = 'mysql:dbname=registration;host=localhost';
-	$user = 'root';
-	$password = '';
-	$db = new PDO($dataSource, $user, $password);
-	$db -> exec("INSERT INTO form(username, password, email) VALUES ('$username', MD5('$password'), '$email')");
+//	}
+$connection = mysqli_connect("localhost", "root", "", "registration");
+mysqli_query($connection, "INSERT INTO regist(username, password, email) VALUES ('$username', MD5('$password'), '$email')");
 ?>
