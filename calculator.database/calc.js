35cbf1069681a1c23641cb62dd4c40cb3bd7c8d8
vermillion23 <den4ik.frolov@gmail.com>
2019-11-13 17:51:36 +0300

    Loop is added for displaying 5 previous operations on the page after clicking "Show history" button.

--- calculator.database/calc.js
+++ calculator.database/calc.js
@@ -16,11 +16,16 @@ function history() {
     event.preventDefault();
 
     let result = fetch("history.php", { method: "POST", body: new FormData(document.forms[0]) })
-    .then(response => response.json())
-    .then(json => {
+        .then(response => response.json())
+        .then(json => {
+            var str = "";
+            var i;
 
-    document.querySelector(".history_field").innerHTML =
-    json[1].operand_1 + " " + json[1].operator + " " + json[1].operand_2 + "=" + " " + json[1].result;
-    })
+            for (i = 0; i < json.length; i++) {
+                str += json[i].operand_1 + " " + json[i].operator + " " + json[i].operand_2 + "=" + " " + json[i].result + "<br>";
+            }
+
+            document.querySelector(".history_field").innerHTML = str;
+
+        })
 }
-
