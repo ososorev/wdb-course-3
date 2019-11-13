798adabb122b649a6369f9fccb2340b31c9d5b4a
vermillion23 <den4ik.frolov@gmail.com>
2019-11-13 17:58:12 +0300

    Extra comments are deleted

--- calculator.database/database.php
+++ calculator.database/database.php
@@ -1,24 +1,4 @@
 <?php
-// $connection=mysqli("localhost", "root",  "", "mybase");
-
-// mysqli_query($connection, "INSERT INTO result(input_result) VALUES('$input result')");
-
-// $input_result = $_REQUEST["input_result"];
-
-// $resourse = mysqli_query($connection, "SELECT * FROM `history` WHERE 1");
-
-// $results = [];
-
-// foreach ($resourse as $row) {
-//     $results[] = $row
-//     $row["id => 1,
-//     "input_result" => "43"]
-
-//     echo $row["input_result];
-//     $row[0]["OperationOne"]
-// }
-
-//Видимо в БД нужны поля: id - operand1 - opreation - operand2 - result - created
 
 class mydatabase {
     private $connection;
@@ -37,18 +17,8 @@ class mydatabase {
         foreach ($input_result as $row) {
             $rows[]=$row;
         }
-
-        //return json_encode($rows);
          return $rows;
     }
 }
-
-// $arr = json_decode($json, true);
-// foreach($arr as $a){
-//     foreach($a as $key => $value){
-//         echo $key . " : " . $value . "<br />";
-//     }
-// }
-
 
 ?>
