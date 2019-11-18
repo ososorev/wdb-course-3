40dff0190f3ef1eee9b62dba8c8c3977b751b458
Артемий <chekanovartemii99@mail.ru>
2019-11-18 22:38:12 +0300

    Update js.js

--- projectNotebook/pageRegistration/js/js.js
+++ projectNotebook/pageRegistration/js/js.js
@@ -7,74 +7,44 @@ function send(event) {
            document.querySelector(".oshibka").innerHTML = outputResult;
     })
 }
-// let forms = {};
+let forms = {};
 
-// (function(){
-//     forms.inputElement=inputElement;
-//     function inputElement (type, name, classList, placeholder, required) {
-//         let input = document.createElement("input");
-//         input.type = type;
-//         input.name = name;
-//         input.classList.add(classList);
-//         input.placeholder = placeholder;
-//         input.required = required;
-//         form.append(input);
-//     };
-// 	forms.buttonElement=buttonElement;
-// 	function buttonElement (classList, type, innerText, onclick) {
-//         let button = document.createElement("button");
-//         button.classList.add(classList);
-//         button.type = type;
-//         button.onclick = onclick;
-//         button.innerText = innerText;
-//         form.append(button);
-//     };
-//     forms.element=element;
-// 	function element (classList, innerText) {
-//         let elementh = document.createElement("div");
-//         elementh.classList.add(classList);
-//         elementh.innerText = innerText;
-//         elementContainer.append(elementh);
-//     };
-// })();
+(function(){
+    forms.inputElement=inputElement;
+    function inputElement (type, name, classList, placeholder, required, form) {
+        let input = document.createElement("input");
+        input.type = type;
+        input.name = name;
+        input.classList.add(classList);
+        input.placeholder = placeholder;
+        input.required = required;
+        form.append(input);
+    };
+	forms.buttonElement=buttonElement;
+	function buttonElement (classList, type, innerText, onclick, form) {
+        let button = document.createElement("button");
+        button.classList.add(classList);
+        button.type = type;
+        button.onclick = onclick;
+        button.innerText = innerText;
+        form.append(button);
+    };
+    forms.element=element;
+	function element (classList, innerText, elementContainer) {
+        let elementh = document.createElement("div");
+        elementh.classList.add(classList);
+        elementh.innerText = innerText;
+        elementContainer.append(elementh);
+    };
+})();
 
-// function createPage() {	
+function createPage() {	
 
-//     let elementContainer = document.createElement("div");
-//     elementContainer.classList.add("container");
-//     document.body.append(elementContainer);
-
-//     forms.element("header", "SUPER NOTEBOOK");
-
-//     let elementContent = document.createElement("div");
-//     elementContent.classList.add("content");
-//     elementContainer.append(elementContent);
-
-//     let form = document.createElement("form");
-//     elementContent.append(form);
-
-// 	forms.inputElement("text", "username", "input", "Username", true);
-// 	forms.inputElement("password", "password", "input", "Password", true);
-// 	forms.inputElement("password", "confirmPassword", "input", "Confirm password", true);
-// 	forms.inputElement("email", "email", "input", "EMail", false);
-// 	forms.buttonElement("button", "submit", "Register", send);
-	
-//     let oshibka = document.createElement("div");
-//     oshibka.classList.add("oshibka");
-//     form.append(oshibka);
-
-//     forms.element("footer", "Copyright by ..., 2016");
-// };
-
-function createPage() {
     let elementContainer = document.createElement("div");
     elementContainer.classList.add("container");
     document.body.append(elementContainer);
 
-    let elementHeader = document.createElement("div");
-    elementHeader.classList.add("header");
-    elementHeader.innerText = "SUPER NOTEBOOK";
-    elementContainer.append(elementHeader);
+    forms.element("header", "SUPER NOTEBOOK", elementContainer);
 
     let elementContent = document.createElement("div");
     elementContent.classList.add("content");
@@ -83,51 +53,16 @@ function createPage() {
     let form = document.createElement("form");
     elementContent.append(form);
 
-    let inputUsername = document.createElement("input");
-    inputUsername.placeholder = "Username";
-    inputUsername.classList.add("input");
-    inputUsername.name = "username";
-    inputUsername.type = "text";
-    //inputUsername.innerHTML = "<?php echo $_SESSION['newsession'] ?>";
-    inputUsername.required = true;
-    form.append(inputUsername);
-
-    let inputPassword = document.createElement("input");
-    inputPassword.placeholder = "Password";
-    inputPassword.classList.add("input");
-    inputPassword.name = "password";
-    inputPassword.type = "password";
-    inputPassword.required = true;
-    form.append(inputPassword);
-
-    let inputConfirmPassword = document.createElement("input");
-    inputConfirmPassword.placeholder = "Confirm password";
-    inputConfirmPassword.classList.add("input");
-    inputConfirmPassword.name = "confirmPassword";
-    inputConfirmPassword.type = "password";
-    inputConfirmPassword.required = true;
-    form.append(inputConfirmPassword);
-
-    let inputEMail = document.createElement("input");
-    inputEMail.placeholder = "EMail";
-    inputEMail.classList.add("input");
-    inputEMail.classList.add("email");
-    inputEMail.name = "email";
-    form.append(inputEMail);
-
-    let button = document.createElement("button");
-    button.classList.add("button");
-    button.type = "submit";
-    button.innerText = "Register";
-    button.onclick = send;
-    form.append(button);
-
+	forms.inputElement("text", "username", "input", "Username", true, form);
+	forms.inputElement("password", "password", "input", "Password", true, form);
+	forms.inputElement("password", "confirmPassword", "input", "Confirm password", true, form);
+	forms.inputElement("email", "email", "input", "EMail", false, form);
+	forms.buttonElement("button", "submit", "Register", send, form);
+	
     let oshibka = document.createElement("div");
     oshibka.classList.add("oshibka");
     form.append(oshibka);
 
-    let elementFooter = document.createElement("div");
-    elementFooter.classList.add("footer");
-    elementFooter.innerText = "Copyright by ..., 2016 ";
-    elementContainer.append(elementFooter);
-}
+    forms.element("footer", "Copyright by ..., 2016", elementContainer);
+};
+
