48f32e6693b3f95a064ee3cc73b4acfd1489ffa9
Артемий <chekanovartemii99@mail.ru>
2019-11-18 23:24:44 +0300

    переделал страничку авторизации

--- projectNotebook/pageAuthorization/js/js.js
+++ projectNotebook/pageAuthorization/js/js.js
@@ -1,66 +1,7 @@
-document.addEventListener("DOMContentLoaded", createPage);
-
 function register(){
     window.location.href = "http://localhost/wdb-course-3/projectNotebook/pageRegistration/index.html";
 }
 
 function login(){
     
-}
-
-function createPage() {
-    let elementContainer = document.createElement("div");
-    elementContainer.classList.add("container");
-    document.body.append(elementContainer);
-
-    let elementHeader = document.createElement("div");
-    elementHeader.classList.add("header");
-    elementHeader.innerText = "SUPER NOTEBOOK";
-    elementContainer.append(elementHeader);
-
-    let elementContent = document.createElement("div");
-    elementContent.classList.add("content");
-    elementContainer.append(elementContent);
-
-    let form = document.createElement("form");
-    form.id = "form";
-    elementContent.append(form);
-
-    let inputUsername = document.createElement("input");
-    inputUsername.placeholder = "Username";
-    inputUsername.classList.add("input");
-    inputUsername.name = "username";
-    inputUsername.type = "text";
-    inputUsername.required = true;
-    form.append(inputUsername);
-
-    let inputPassword = document.createElement("input");
-    inputPassword.placeholder = "Password";
-    inputPassword.classList.add("input");
-    inputPassword.name = "password";
-    inputPassword.type = "password";
-    inputPassword.required = true;
-    form.append(inputPassword);
-
-    let buttonLogin = document.createElement("button");
-    buttonLogin.classList.add("button");
-    buttonLogin.type = "submit";
-    buttonLogin.innerText = "Login";
-    //buttonLogin.onclick = send;
-    form.append(buttonLogin);
-
-    let buttonregister = document.createElement("button");
-    buttonregister.classList.add("button");
-    buttonregister.innerText = "Register";
-    buttonregister.onclick = register;
-    form.append(buttonregister);
-
-    let oshibka = document.createElement("div");
-    oshibka.classList.add("oshibka");
-    form.append(oshibka);
-
-    let elementFooter = document.createElement("div");
-    elementFooter.classList.add("footer");
-    elementFooter.innerText = "Copyright by ..., 2016 ";
-    elementContainer.append(elementFooter);
 }
--- projectNotebook/pageAuthorization/php/index.php
+++ projectNotebook/pageAuthorization/php/index.php
@@ -1,17 +1,32 @@
 <?php
-    require_once "sql.php";
+    //require_once "sql.php";
     $username = $_REQUEST["username"];
     $password = $_REQUEST["password"];
-    $confirmPassword = $_REQUEST["confirmPassword"];
-    $email = $_REQUEST["email"];
-    if (!empty($username) && !empty($password) && !empty($confirmPassword)) {
-        if ($password == $confirmPassword) {
-            sql::queryInsert("INSERT INTO user(username, password, email)
-                VALUES ('$username', '$password', '$email')");
-        } else {
-            echo "Проверьте введенный пароль!";
-        }
-    } else {
-        echo "Заполните все обязательные поля:<br />Username, Password, Confirm password";
-    }
 ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="notebook" content="width=device-width, initial-scale=1.0">
+        <title>notebook</title>
+        <link rel="stylesheet" href="../style/style.css">
+        <script src="../js/js.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                SUPER NOTEBOOK
+            </div>
+            <form class="content">
+                <input class="input" type="text" name="username" placeholder="Username" required />
+                <input class="input" type="password" name="password" placeholder="Password" required />
+                <button class="button" type="submit">Login</button>
+                <button class="button" onclick="register()">Register</button>
+            </form>
+            <div class="oshibka"></div>
+            <div class="footer">
+                    Copyright by ..., 2016
+            </div>
+        </div>
+    </body>
+</html>
