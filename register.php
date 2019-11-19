9bed2f8bcffcc330ff75dd88d306c82576a04a16
FivePood <ps220790@gmail.com>
2019-11-19 22:06:32 +0300

    с валидацией

--- register.php
+++ register.php
@@ -9,25 +9,25 @@
 		echo ($result);
 	}
 	function validate() {					
-//		if ($password == $confirmPassword) {
-//			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		if ($password === $confirmPassword) {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				$result = "Successful!";
-//			} 
-//			else if (1==1){
-//				$result = "Invalid email";
-//			}
-//		else {
-//			$result = "Invalid password";
-//		}
+			} 
+			else{
+				$result = "Invalid email";
+			}
+		}
+		else {
+			$result = "Invalid password";
+		}
 		return($result);
-//		}
 	}
 
 $connection = mysqli_connect("localhost", "root", "", "registration");
 mysqli_query($connection, "INSERT INTO regist(username, password, email) VALUES ('$username', MD5('$password'), '$email')");
 
 	if ($result == "Successful!"){ 
-		echo "<META HTTP-EQUIV='Refresh' content='2; URL=index.html'>";
+		echo "<META HTTP-EQUIV='Refresh' content='2; URL=index.php'>";
 		exit(); 
 	} 
 ?>
