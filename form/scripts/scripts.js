d094ba54b8d21f7a8e6a8741ea5894f320827457
BillyBones <useemy@gmail.com>
2019-11-30 21:56:22 +0300

    Удалил лишние, отсортировал домашние задания

--- form/scripts/scripts.js
+++ form/scripts/scripts.js
@@ -1,2 +1,7 @@
 // JavaScript Document
 document.addEventListener("DOMContentLoaded", createPage);
+
+//function send(event){
+//	event.preventDefault();
+//	fetch("users_check.php",{method:"POST", body:new FormData(document.forms[0])}).then(response=>response.text()).then(text=>{document.getElementById("strip_start").innerHTML=text;});
+//};
