838234f20b0d7d13c7344464832edbacf0100f2c
olikik <olgam299@gmail.com>
2019-12-07 15:10:22 +0300

    Стандартная форма авторизации, работает

--- notebook/app/users.php
+++ notebook/app/users.php
@@ -3,15 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class users extends Model
+class users extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
     protected $fillable =['name','email','password'];
     function __construct(array $attributes = [])
     {
-        print_r($attributes);
         parent::__construct($attributes);
-
-
     }
 }
